169b563964010dfec9df79fbec0319ae5bd99c4a
Kuroissaint <kurobasaint04@gmail.com>
2025-12-26 00:52:33 +0700

    Delete game directory

--- game/intent/StartGameIntent.java
+++
@@ -1,9 +0,0 @@
-package com.monster.game.intent;
-
-public class StartGameIntent implements GameIntent {
-    public String username;
-
-    public StartGameIntent(String username) {
-        this.username = username;
-    }
-}
